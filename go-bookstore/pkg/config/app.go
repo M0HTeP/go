2509279f41fae7257f461f823be92479dc7a7cbc
Artem Ryazanov <79083447+M0HTeP@users.noreply.github.com>
2022-04-14 22:18:16 +0300

    Update app.go

--- go-bookstore/pkg/config/app.go
+++ go-bookstore/pkg/config/app.go
@@ -5,18 +5,21 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
+// создание базы данных
 var (
 	db *gorm.DB
 )
 
+// функция подключения к базе данных
 func Connect() {
-	d, err := gorm.Open("mysql", "MoHTeP:MOHTEP@/simplerest?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open("mysql", "MoHTeP:MOHTEP@/simplerest?charset=utf8&parseTime=True&loc=Local") // данные для подключения. err - ошибка
 	if err != nil {
-		panic(err)
+		panic(err) // остановка функции
 	}
-	db = d
+	db = d // если все нормально - база данных подключена и назначена переменной
 }
 
+// функция для получения базы данных из переменной
 func GetDB() *gorm.DB {
 	return db
 }
