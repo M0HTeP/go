b9bc9e7ddfa55d9128a5f2b903bbe797eabeb88f
Artem Ryazanov <79083447+M0HTeP@users.noreply.github.com>
2022-04-14 22:18:25 +0300

    Update bookstore-routes.go

--- go-bookstore/pkg/routes/bookstore-routes.go
+++ go-bookstore/pkg/routes/bookstore-routes.go
@@ -5,10 +5,11 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// Создаем необходимые нам хендлеры(не хватает вокабуляра для того чтобы написать это слово на русский манер) для функций
 var RegisterBookStoreRoutes = func(router *mux.Router) {
-	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
-	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
-	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
-	router.HandleFunc("/book{bookId}/", controllers.UpdateBook).Methods("PUT")
-	router.HandleFunc("/book{bookId}/", controllers.DeleteBook).Methods("DELETE")
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")           // функция создания книги
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")               // функция получения информации о книгах
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")   // функция получения информации о книге по ее Id
+	router.HandleFunc("/book{bookId}/", controllers.UpdateBook).Methods("PUT")    // функция обновления информации о книге под определенным ID
+	router.HandleFunc("/book{bookId}/", controllers.DeleteBook).Methods("DELETE") // функция удаления книги
 }
