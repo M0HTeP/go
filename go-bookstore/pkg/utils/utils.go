f7ff9edd86462f13b51f246d8f85f819f790268d
Artem Ryazanov <79083447+M0HTeP@users.noreply.github.com>
2022-04-14 22:18:27 +0300

    Update utils.go

--- go-bookstore/pkg/utils/utils.go
+++ go-bookstore/pkg/utils/utils.go
@@ -6,9 +6,9 @@ import (
 	"net/http"
 )
 
-func ParseBody(r *http.Request, x interface{}) {
-	if body, err := ioutil.ReadAll(r.Body); err == nil {
-		if err := json.Unmarshal([]byte(body), x); err != nil {
+func ParseBody(r *http.Request, x interface{}) { // получение запроса
+	if body, err := ioutil.ReadAll(r.Body); err == nil { // чтение "тела" запроса
+		if err := json.Unmarshal([]byte(body), x); err != nil { // в случае, если в запросе ошибок нет - парсим данные в формате json
 			return
 		}
 	}
