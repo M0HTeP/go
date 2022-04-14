b1c03f284cac7fa1c2411ca5c1ac0f05c757ce72
Artem Ryazanov <79083447+M0HTeP@users.noreply.github.com>
2022-04-14 22:18:11 +0300

    Update main.go

--- go-bookstore/cmd/main/main.go
+++ go-bookstore/cmd/main/main.go
@@ -9,9 +9,10 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
+// главная функция
 func main() {
-	r := mux.NewRouter()
-	routes.RegisterBookStoreRoutes(r)
-	http.Handle("/", r)
-	log.Fatal(http.ListenAndServe("localhost:9010", r))
+	r := mux.NewRouter()                                // создаем новый инстанс
+	routes.RegisterBookStoreRoutes(r)                   // Для нашего инстанса cоздаем необходимые нам хендлеры(не хватает вокабуляра для того чтобы написать это слово на русский манер) для функций
+	http.Handle("/", r)                                 // регистрируем хендлер (?)
+	log.Fatal(http.ListenAndServe("localhost:9010", r)) // в слечае ошибке на назначенном порте - заносим ее в лог
 }
