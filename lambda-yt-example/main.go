6cefc69c777333924f337bf96405f28320f4eba7
Artem Ryazanov <79083447+M0HTeP@users.noreply.github.com>
2022-04-15 14:25:45 +0300

    Update main.go

--- lambda-yt-example/main.go
+++ lambda-yt-example/main.go
@@ -4,20 +4,23 @@ import (
 	"fmt"
 	"github.com/aws/aws-lambda-go/lambda"
 )
-
+    //    создаем структуру данных
 type MyEvent struct {
-	Name string `json:"what is your name?"`
-	Age  int    `json:"How old are you?"`
+	Name string `json:"what is your name?"`    //    Имя
+	Age  int    `json:"How old are you?"`    //    Возраст
 }
 
 type MyResponse struct {
-	Message string `json:"Answer:"`
+	Message string `json:"Answer:"`    //    Вид ответа
 }
 
 func HandleLambdaEvent(event MyEvent) (MyResponse, error) {
-	return MyResponse{Message: fmt.Sprintf("%s is %d years old", event.Name, event.Age)}, nil
+	return MyResponse{Message: fmt.Sprintf("%s is %d years old", event.Name, event.Age)}, nil    //    Отвечаем на сообщение
 }
 
 func main() {
-	lambda.Start(HandleLambdaEvent)
+	lambda.Start(HandleLambdaEvent)    //    запускаем функцию
 }
+
+// aws iam create-role --role-name lambda-ex --assume-role-policy-document file://trust-policy.json
+// Не понял, как заставить этот код работать...
