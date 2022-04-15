33f9803beb7006c85a405ae9d0d1f5338b3169a7
Artem Ryazanov <79083447+M0HTeP@users.noreply.github.com>
2022-04-15 21:34:49 +0300

    Update handlers.go

--- go-serverless-yt/pkg/handlers/handlers.go
+++ go-serverless-yt/pkg/handlers/handlers.go
@@ -14,20 +14,20 @@ var ErrorMethodNotAllowed = "method not allowed"
 type ErrorBody struct{
 	ErrorMsg *string `json:"error,omitempty"`
 }
-
+	//	функция вывода информации о пользователе
 func GetUser(req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI)(
 	*events.APIGatewayProxyResponse, error,
 ){
-
+	//	получение информации о электронной почте
 	email := req.QueryStringParameters["email"]
 	if len(email) > 0 {
-		result, err := user.FetchUser(email, tableName, dynaClient)
+		result, err := user.FetchUser(email, tableName, dynaClient)	//	парсим информацию
 		if err!= nil {
-			return apiResponse(http.StatusBadRequest, ErrorBody{aws.String(err.Error())})
+			return apiResponse(http.StatusBadRequest, ErrorBody{aws.String(err.Error())})	//	в случае ошибки отправляем статус и описание ошибки
 		}
-		return apiResponse(http.StatusOK, result)
+		return apiResponse(http.StatusOK, result)	//	если все нормально - отправляем результат
 	}
-
+	//	получаем информацию о ВСЕХ пользователях
 	result, err := user.FetchUsers(tableName, dynaClient)
 	if err!= nil {
 		return apiResponse(http.StatusBadRequest, ErrorBody{
@@ -37,23 +37,23 @@ func GetUser(req events.APIGatewayProxyRequest, tableName string, dynaClient dyn
 	return apiResponse(http.StatusOK, result)
 
 }
-
+	//	функция создания нового пользователя
 func CreateUser(req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI)(
 	*events.APIGatewayProxyResponse, error,
 ){
-	result, err := user.CreateUser(req, tableName, dynaClient)
+	result, err := user.CreateUser(req, tableName, dynaClient)	//	создаем нового пользователя с заданными параметрами
 	if err!=nil {
-		return apiResponse(http.StatusBadRequest, ErrorBody{
+		return apiResponse(http.StatusBadRequest, ErrorBody{	//	отлавливаем ошибки
 			aws.String(err.Error()),
 		})
 	}
 	return apiResponse(http.StatusCreated, result)
 }
-
+	//	функция обновления информации о пользователе
 func UpdateUser(req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI)(
 	*events.APIGatewayProxyResponse, error,
 ){
-	result, err := user.UpdateUser(req, tableName, dynaClient)
+	result, err := user.UpdateUser(req, tableName, dynaClient)	//	обновляем информацию
 	if err!= nil {
 		return apiResponse(http.StatusBadRequest, ErrorBody{
 			aws.String(err.Error()),
@@ -61,7 +61,7 @@ func UpdateUser(req events.APIGatewayProxyRequest, tableName string, dynaClient 
 	}
 	return apiResponse(http.StatusOK, result)
 }
-
+	//	функция удаления пользователя
 func DeleteUser(req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI)(
 	*events.APIGatewayProxyResponse, error,
 ){
@@ -74,7 +74,7 @@ func DeleteUser(req events.APIGatewayProxyRequest, tableName string, dynaClient 
 	}
 	return apiResponse(http.StatusOK, nil)
 }
-
+	//	функция для определения допустимости метода
 func UnhandledMethod()(*events.APIGatewayProxyResponse, error){
 	return apiResponse(http.StatusMethodNotAllowed, ErrorMethodNotAllowed)
 }
