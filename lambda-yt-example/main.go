0636b3fba3993d22922195a25a65460b9a10b9f8
Artem Ryazanov <79083447+M0HTeP@users.noreply.github.com>
2022-04-15 14:20:04 +0300

    Update main.go

--- lambda-yt-example/main.go
+++ lambda-yt-example/main.go
@@ -1,29 +1,23 @@
 package main
 
-import(
+import (
 	"fmt"
-	"ginhub.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type MyEvent struct{
-Name string `json:"what is your name?"`
-Age int `json:"How old are you?"`
-
+type MyEvent struct {
+	Name string `json:"what is your name?"`
+	Age  int    `json:"How old are you?"`
 }
 
-type MyResponse struct{
-Message string `json:"Answer:"`
-
+type MyResponse struct {
+	Message string `json:"Answer:"`
 }
 
-
-func HandleLambdaEvent(event MyEvent)(MyResponse, error){
+func HandleLambdaEvent(event MyEvent) (MyResponse, error) {
 	return MyResponse{Message: fmt.Sprintf("%s is %d years old", event.Name, event.Age)}, nil
 }
 
-func main(){
-lambda.Start(HandleLambdaEvent)
-
-
-
+func main() {
+	lambda.Start(HandleLambdaEvent)
 }
