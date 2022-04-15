1f544894ab77688ba2c3dbdb602ed0e2bdaec7fd
Artem Ryazanov <79083447+M0HTeP@users.noreply.github.com>
2022-04-15 14:54:42 +0300

    Update main.go

--- go-fiber-crm-basic/main.go
+++ go-fiber-crm-basic/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"fmt"
+	"github.com/go-fiber-crm-basic/database"
+	"github.com/gofiber/fiber"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Get(GetLeads)
+	app.Get(GetLead)
+	app.Post(NewLead)
+	app.Delete(Delete)
+}
+
+func initDatabase() {
+
+}
+
+func main() {
+	app := fiber.New()
+	setupRoutes(app)
+	app.Listen(3000)
+
+}
