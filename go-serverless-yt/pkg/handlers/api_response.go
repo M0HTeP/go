2d116a56c80439dc3adff65e3cebd0b872c506a3
Artem Ryazanov <79083447+M0HTeP@users.noreply.github.com>
2022-04-15 21:23:07 +0300

    Update api_response.go

--- go-serverless-yt/pkg/handlers/api_response.go
+++ go-serverless-yt/pkg/handlers/api_response.go
@@ -4,12 +4,12 @@ import(
 	"encoding/json"
 	"github.com/aws/aws-lambda-go/events"
 )
-
+	//	функция отрисовки действий с таблицей
 func apiResponse(status int, body interface{})(*events.APIGatewayProxyResponse, error){
-	resp := events.APIGatewayProxyResponse{Headers: map[string]string{"Content-Type":"application/json"}}
-	resp.StatusCode = status 
+	resp := events.APIGatewayProxyResponse{Headers: map[string]string{"Content-Type":"application/json"}}	//	сам ответ
+	resp.StatusCode = status 	//	статус ответа
 
-	stringBody, _ := json.Marshal(body)
-	resp.Body = string(stringBody)
+	stringBody, _ := json.Marshal(body)	//	преобразователь тела запроса
+	resp.Body = string(stringBody)	//	конвертор тела запроса
 	return &resp, nil
 }
