c66c405782547cc0e63474695f096cd409de3b71
Artem Ryazanov <79083447+M0HTeP@users.noreply.github.com>
2022-04-15 15:20:45 +0300

    Update main.go

--- go-fiber-crm-basic/main.go
+++ go-fiber-crm-basic/main.go
@@ -7,14 +7,21 @@ import (
 )
 
 func setupRoutes(app *fiber.App) {
-	app.Get(GetLeads)
-	app.Get(GetLead)
-	app.Post(NewLead)
-	app.Delete(Delete)
+	app.Get("/api/v1/lead", lead.GetLeads)
+	app.Get("/api/v1/lead:id", lead.GetLead)
+	app.Post("/api/v1/lead", lead.NewLead)
+	app.Delete("/api/v1/lead:id", lead.DeleteLead)
 }
 
 func initDatabase() {
-
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3", "leads.db") // открываем БД leads.db
+	if err != nil {
+		panic("faild to connect database")
+	}
+	fmt.Println("Connection opened to database")
+	database.DBConn.AutiMigrate(&lead.Lead{})
+	ftm.Println("Database Migrated")
 }
 
 func main() {
@@ -22,6 +29,6 @@ func main() {
 	initDatabase()
 	setupRoutes(app)
 	app.Listen(3000)
-	defer database.DBConn
+	defer database.DBConn.Close() // после выполнения функции закрыть БД
 
 }
