10615a6170a5d06054be110d58eda54d92e1d642
Artem Ryazanov <79083447+M0HTeP@users.noreply.github.com>
2022-04-15 17:57:31 +0300

    Update lead.go

--- go-fiber-crm-basic/lead/lead.go
+++ go-fiber-crm-basic/lead/lead.go
@@ -7,51 +7,56 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
+//    структура нашей БД
 type Lead struct {
 	gorm.Model
-	Name    string	`json:"name"`
-	Company string	`json:"company"`
-	Email   string	`json:"email"`
-	Phone   int		`json:"phone"`
+	Name    string `json:"name"`
+	Company string `json:"company"`
+	Email   string `json:"email"`
+	Phone   int    `json:"phone"`
 }
 
+//    функция получения всех записей БД
 func GetLeads(c *fiber.Ctx) {
-	db := database.DBConn
-	var leads []Lead
-	db.Find(&leads)
-	c.JSON(leads)
+	db := database.DBConn //    соединяемся с БД
+	var leads []Lead      //    переменная для хранения данных БД
+	db.Find(&leads)       //    ищем значения
+	c.JSON(leads)         //    выводим значения
 }
 
+//    функция получения информации об определенном ID
 func GetLead(c *fiber.Ctx) {
-	id := c.Params("id")
+	id := c.Params("id") //    выцепляем ID из запроса
 	db := database.DBConn
-	var lead Lead
-	db.Find(&lead, id)
+	var lead Lead	//    переменная для хранения объекта БД
+	db.Find(&lead, id) //    ищем объект БД с нужным ID
 	c.JSON(lead)
 }
 
+//    функция создания нового объекта БД
 func NewLead(c *fiber.Ctx) {
 	db := database.DBConn
-	lead := new(Lead)
-	if err := c.BodyParser(lead); err != nil {
-		c.Status(503).Send(err)
+	lead := new(Lead)                          //    переменная с новым объектом
+	if err := c.BodyParser(lead); err != nil { //    парсим тело запроса
+		c.Status(503).Send(err) //    в случае ошибки отправляем статус "ошибка"
 		return
 	}
 	db.Create(&lead)
 	c.JSON(lead)
 }
 
+//    функция удаления объекта БД
 func DeleteLead(c *fiber.Ctx) {
 	id := c.Params("id")
 	db := database.DBConn
 
 	var lead Lead
-	db.First(&lead, id)
-	if lead.Name == ""{
-		c.Status(500).Send("No lead found with ID")
+	db.First(&lead, id)	//    Ищем первое совпадение объекта БД с нужным ID
+	if lead.Name == "" {
+		c.Status(500).Send("No lead found with ID")	//    если такого ID не существует так и говорим
 		return
 	}
-	db.Delete(&lead)
-	c.Send("Lead successfully deleted")
+	db.Delete(&lead)	//    удаляем найденный объект БД
+	c.Send("Lead successfully deleted")	//    отчитываемся о выполнении
 
 }
