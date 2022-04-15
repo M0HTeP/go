3432495805056da11cfe26baae3711d38eccd656
Artem Ryazanov <79083447+M0HTeP@users.noreply.github.com>
2022-04-15 12:11:56 +0300

    Update main.go

--- slack-age-bot/main.go
+++ slack-age-bot/main.go
@@ -7,3 +7,10 @@ import (
 	"log"
 	"os"
 )
+
+func main() {
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-3398622637444-3420057801264-83ngRBGlfJOOceesO2TOG8nz")
+	os.Setenv("SLACK_APP_TOKEN", "xapp-1-A03B7L3NV55-3389640585750-9324c170eece11c47d83bf80048b3ce5af5fad4c025df06391de3b1a0378342a")
+
+	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+}
