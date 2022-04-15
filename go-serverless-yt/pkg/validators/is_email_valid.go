9eb926dae44b6982eda20913c5dbe1c51fa38ff4
Artem Ryazanov <79083447+M0HTeP@users.noreply.github.com>
2022-04-15 20:47:51 +0300

    Update is_email_valid.go

--- go-serverless-yt/pkg/validators/is_email_valid.go
+++ go-serverless-yt/pkg/validators/is_email_valid.go
@@ -2,10 +2,11 @@ package validators
 
 import "regexp"
 
-func IsEmailValid(email string) bool {
+func IsEmailValid(email string) bool {	//	проверка электронной почты на валидность
+		//	прописываем какие символы должна содержать почта
 	var rxEmail = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]{1,64}@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
-	if len(email) < 3 || len(email) > 254 || !rxEmail.MatchString(email){
+	if len(email) < 3 || len(email) > 254 || !rxEmail.MatchString(email){	//	ограничение по длине
 		return false
 	}
 
